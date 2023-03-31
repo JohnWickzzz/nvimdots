13ff37e64e8daf036d48e00f43adb28c9a905187
leebo <buptlibo@gmail.com>
2023-03-31 14:30:02 +0800

    personal plugin

---
+++ lua/modules/plugins/custom.lua
@@ -0,0 +1,18 @@
+local custom = {}
+
+custom["mg979/vim-visual-multi"] = {
+	lazy = false,
+	-- event = "BufRead",
+	config = require("custom.vim-visual-multi"), -- Require that config
+}
+
+custom["kylechui/nvim-surround"] = {
+	version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end}
+
+return custom
