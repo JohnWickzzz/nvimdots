658da697ee93b8cfcee106ef817f8097a18877ba
leebo <buptlibo@gmail.com>
2023-12-20 13:02:17 +0800

    fix: nvim-java envnt

--- lua/modules/plugins/custom.lua
+++ lua/modules/plugins/custom.lua
@@ -38,7 +38,7 @@ custom["nvim-java/nvim-java"] = {
 	lazy = true,
 	ft = "java",
 	config = require("custom.nvim-java"),
-	-- event = "LspAttach",
+	event = "LspAttach",
 	dependencies = {
 		"nvim-java/lua-async-await",
 		"nvim-java/nvim-java-core",
