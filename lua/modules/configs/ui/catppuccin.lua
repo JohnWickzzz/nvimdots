0987802a1e04e6a7d6f0806e63fe886e8175a02c
leebo <buptlibo@gmail.com>
2024-01-05 14:28:21 +0800

    feat: update git-conflict bg color

--- lua/modules/configs/ui/catppuccin.lua
+++ lua/modules/configs/ui/catppuccin.lua
@@ -133,6 +133,10 @@ return function()
 						bg = transparent_background and cp.none or cp.mantle,
 					},
 
+					-- For git-conflict
+					DiffAdd = { bg = "#008080" },
+					DiffText = { bg = "#B33076", fg = cp.base },
+
 					-- For fidget
 					FidgetTask = { bg = cp.none, fg = cp.surface2 },
 					FidgetTitle = { fg = cp.blue, style = { "bold" } },
