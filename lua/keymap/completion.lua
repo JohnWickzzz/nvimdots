7ec76e7a82a4745ff83c7f45d86df455bce351bd
leebo <buptlibo@gmail.com>
2024-01-13 21:35:08 +0800

    feat: add keymap for lsp declaration

--- lua/keymap/completion.lua
+++ lua/keymap/completion.lua
@@ -49,6 +49,9 @@ function mapping.lsp(buf)
 			:with_desc("lsp: Code action for cursor"),
 		["n|gd"] = map_cr("Glance definitions"):with_silent():with_buffer(buf):with_desc("lsp: Preview definition"),
 		["n|gD"] = map_cr("Lspsaga goto_definition"):with_silent():with_buffer(buf):with_desc("lsp: Goto definition"),
+		["n|gu"] = map_callback(function()
+			vim.lsp.buf.declaration()
+		end):with_desc("lsp: Signature help"),
 		["n|gh"] = map_cr("Glance references"):with_silent():with_buffer(buf):with_desc("lsp: Show reference"),
 		["n|gm"] = map_cr("Glance implementations")
 			:with_silent()
