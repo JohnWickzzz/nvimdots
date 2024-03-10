abc58025c33486d41bbf345bd591a1547e372217
leebo <buptlibo@gmail.com>
2024-03-10 15:43:28 +0800

    fix: interestingwords config

--- lua/modules/configs/custom/interestingwords.lua
+++ lua/modules/configs/custom/interestingwords.lua
@@ -3,6 +3,7 @@ return function()
 		colors = { "#aeee00", "#BA5151", "#1976D2", "#b88844", "#ffa724", "#FF6D00", "#3F51B5" },
 		search_count = true,
 		navigation = true,
+		scroll_center = true,
 		search_key = "<leader>m",
 		cancel_search_key = "<leader>M",
 		color_key = "<leader>k",
